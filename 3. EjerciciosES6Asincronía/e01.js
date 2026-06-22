a812347a46f3d1f3386d682bdb30673a6590947c
Rafal <afarfuenla@hotmail.com>
2026-06-22 06:04:40 +0200

    Ejercicio 1 completado

--- 3. EjerciciosES6Asincronía/e01.js
+++ 3. EjerciciosES6Asincronía/e01.js
@@ -3,9 +3,25 @@
 
 const game = {title: 'The Last of Us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
+const { title, gender, year} = game
+
+console.log(title);
+console.log(year);
+console.log(gender);
+console.log();
+
+
 //! 1.2 En base al siguiente javascript, usa destructuring para crear 3 variables llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente imprimelo por consola.
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruit1, fruit2, fruit3] = fruits
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+console.log();
+
 
 //! 1.3 En base al siguiente javascript, usa destructuring para crear 2 variables igualandolo a la función e imprimiendolo por consola.
 
@@ -13,6 +29,21 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'} 
 };
 
+const {name, race} = animalFunction()
+console.log(name);
+console.log(race);
+console.log();
+
+
 //! 1.4 En base al siguiente javascript, usa destructuring para crear las variables name y itv con sus respectivos valores. Posteriormente crea 3 variables usando igualmente el destructuring para cada uno de los años y comprueba que todo esta bien imprimiendolo.
 
 const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const { name, itv} = car
+const [anio1, anio2, anio3] = itv
+
+console.log(name);
+console.log(itv);
+console.log(anio1);
+console.log(anio2);
+console.log(anio3);
