9e1da142e5e9b96b96dcf1d29801f13bfef246e4
Rafal <afarfuenla@hotmail.com>
2026-06-22 13:56:13 +0200

    Segundo ejercicio de E06 terminado

--- 3. EjerciciosES6Asincronía/e01.js
+++ 3. EjerciciosES6Asincronía/e01.js
@@ -37,12 +37,12 @@ console.log();
 
 //! 1.4 En base al siguiente javascript, usa destructuring para crear las variables name y itv con sus respectivos valores. Posteriormente crea 3 variables usando igualmente el destructuring para cada uno de los años y comprueba que todo esta bien imprimiendolo.
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = {nameCar: 'Mazda 6', itv: [2015, 2011, 2020] }
 
-const { name, itv} = car
+const {nameCar, itv} = car
 const [anio1, anio2, anio3] = itv
 
-console.log(name);
+console.log(nameCar);
 console.log(itv);
 console.log(anio1);
 console.log(anio2);
