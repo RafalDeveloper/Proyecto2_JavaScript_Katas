098743f91cb2a9dd335d82dd6cbbe8a0843a04dd
Rafal <afarfuenla@hotmail.com>
2026-06-24 21:29:36 +0200

    Ejercicio 7 completado

--- 3. EjerciciosES6Asincronía/e06.js
+++ 3. EjerciciosES6Asincronía/e06.js
@@ -2,18 +2,34 @@
 //! 6.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de los alumnos usando la función .reduce().
 
 const exams = [
-    {name: 'Yuyu Cabeza Crack', score: 5},
-    {name: 'Maria Aranda Jimenez', score: 1},
-    {name: 'Cristóbal Martínez Lorenzo', score: 6},
-    {name: 'Mercedez Regrera Brito', score: 7},
-    {name: 'Pamela Anderson', score: 3},
-    {name: 'Enrique Perez Lijó', score: 6},
-    {name: 'Pedro Benitez Pacheco', score: 8},
-    {name: 'Ayumi Hamasaki', score: 4},
-    {name: 'Robert Kiyosaki', score: 2},
-    {name: 'Keanu Reeves', score: 10}
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
 ];
+
+const suma = exams.reduce((acumulador, sumas) => acumulador + sumas.score, 0);
+console.log("Ejercicio 1:");
+console.log(suma);
+
 
 //!  6.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los alumnos que esten aprobados usando la función .reduce().
 
+const sumaAprobados = exams
+  .filter((nota) => nota.score > 5)
+  .reduce((acumulado, suma) => acumulado + suma.score, 0);
+  console.log("Ejercicio 2:");
+  console.log(sumaAprobados)
+
+
 //! 6.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+
+const mediaNotas = exams.reduce((acumulador,sumas) => acumulador+ sumas.score,0)
+console.log("Ejercicio 3:");
+console.log(mediaNotas / exams.length);
